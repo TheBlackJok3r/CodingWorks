97b7c808644067dc31ea746fce9527694cc68bef
TheBlackJok3r <68962009+TheBlackJok3r@users.noreply.github.com>
2021-04-03 19:44:36 +0200

    Repairing some bugs

--- Python/ArraySpliting.py
+++ Python/ArraySpliting.py
@@ -1,30 +1,33 @@
 from random import random
 from random import randint
 
-n=100
-A=[]
+n = 10
+A = []
+
 
 def RandTab():
     for _ in range(n):
-        A.append(randint(0,10))
+        A.append(randint(0, 10))
+
 
 def Split(A, start=0, end=n-1):
-    pivot=randint(start,end)
-    spiv=A[pivot]
-    print(pivot)
-    print(A[pivot])
-    i=0
-    while start<end:
-        if A[start]<=spiv:
-            start+=1
-        if A[end]>spiv:
-            end-=1
-        if A[end]<=spiv and A[start]>spiv:
-            A[start],A[end]=A[end],A[start]
-            if start>pivot:
-                pivot=start
-            elif end<pivot:
-                pivot=end
+    pivot = randint(start, end)
+    spiv = A[pivot]
+    print(pivot, spiv)
+    i = 0
+    while start+1 < end:
+        if A[start] <= spiv:
+            start += 1
+        if A[end] > spiv:
+            end -= 1
+        if A[end] <= spiv and A[start] > spiv and start < end:
+            A[start], A[end] = A[end], A[start]
+            if start > pivot:
+                pivot = start
+            elif end < pivot:
+                pivot = end
+    return pivot
+
 
 RandTab()
 print(A)
