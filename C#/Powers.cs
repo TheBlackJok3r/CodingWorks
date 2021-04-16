4191e1bfb240b15138fe816b1dcbf8d6425add77
TheBlackJok3r <68962009+TheBlackJok3r@users.noreply.github.com>
2021-04-17 00:48:40 +0200

    Add files via upload

---
+++ C#/Powers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C__projects
+{
+    class Powers
+    {
+        static long pow(long power, long num)
+        {
+            if (power == 2)
+            {
+                return num * num;
+            }
+            else if (power == 3)
+            {
+                return num * num * num;
+            }
+            else if (power % 2 == 0)
+            {
+                return pow(power / 2, num) * pow(power / 2, num);
+            }
+            else if (power % 3 == 0)
+            {
+                return pow((power - 1) / 2, num) * pow((power - 1) / 2, num) * num;
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(pow(18, 10));
+        }
+    }
+}
