9854e01e82d6c491e32eebeb3acde5a6daa14f94
TheBlackJok3r <68962009+TheBlackJok3r@users.noreply.github.com>
2021-06-01 20:14:08 +0200

    School Multiplication Algorithm
    Simple multiplication algorithm known from school

---
+++ Python/SchoolMultiplication.py
@@ -0,0 +1,14 @@
+from random import paretovariate
+
+
+import math
+
+
+def Mult(a, b, i=1):
+    if a == 0 or b == 0:
+        return 0
+    bStr = str(b)
+    return Mult(a, math.floor(b/10), i*10) + a*int(bStr[len(bStr)-1])*i
+
+
+print(Mult(11, 110))
