3b8c1dfcd9cc3bf423f91d3ab1052d4183057ab1
TheBlackJok3r <68962009+TheBlackJok3r@users.noreply.github.com>
2021-07-07 11:07:34 +0200

    Add converting from base to dec system

--- Python/Base.py
+++ Python/Base.py
@@ -5,3 +5,17 @@ def Base(base=10, num=0):
     if rest == 0:
         return digits[x]
     return Base(base, rest) + digits[x]
+
+
+
+# works only with lowercase letters
+def BaseToDec(num, fromBase):
+    a = 0
+    for i in num:
+        a *= fromBase
+        b = ord(i)
+        if b > 57:
+            a += int(b-87)
+        else:
+            a += int(chr(b))
+    return a
