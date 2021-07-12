7665eae3d24886ecb6b0aea7a2d29ce8bee59ac9
TheBlackJok3r <68962009+TheBlackJok3r@users.noreply.github.com>
2021-07-13 01:39:05 +0200

    Barrels on the inclined plane sorting algorithm
    Special conditions of the task: 
    There are 3 barrel colors
    Must always take 3 adjacent barrels while sorting
    Task is to sort them by color in any final pattern, but all barrels, that are in the same color, must stick to each other.

---
+++ Python/ColoredBarrels.py
@@ -0,0 +1,34 @@
+def helper(s):
+    A = []
+    last = ''
+    result = True
+    for i in s:
+        if last == i:
+            continue
+        elif last != i and i not in A:
+            A.append(i)
+            last = i
+        else:
+            result = False
+            break
+    return result
+
+
+def barrels(pattern, acc=0):
+    if helper(pattern):
+        return f'The result pattern is: {pattern}'
+    else:
+        pat = pattern[0:acc] + pattern[acc+3:len(pattern)] + pattern[acc:acc+3]
+        if acc < len(pattern)-4:
+            return f'{pat} \n {barrels(pat, acc+1)}'
+        else:
+            return f'{pat} \n {barrels(pat, 0)}'
+
+
+def main():
+    with open('Python/coloredBarrels.txt') as file:
+        text = file.read().split(' ')
+        print(barrels(text))
+
+
+main()
